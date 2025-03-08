66522e333002dfb87ad85d88a2f2704fab2cadde
alif <alifa.m@ui.ac.id>
2025-03-08 21:28:07 +0700

    feat: PBI 9

--- genius/src/main/java/com/propensi/genius/rest/restservice/InstansiRestService.java
+++ genius/src/main/java/com/propensi/genius/rest/restservice/InstansiRestService.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import com.propensi.genius.rest.restdto.request.AddInstansiRequestDTO;
 import com.propensi.genius.rest.restdto.response.InstansiResponseDTO;
+import com.propensi.genius.rest.restdto.request.UpdateInstansiRequestDTO;
 
 public interface InstansiRestService {
     List<InstansiResponseDTO> getAllInstansi();
 
     InstansiResponseDTO addInstansi(AddInstansiRequestDTO requestDTO);
+    InstansiResponseDTO getInstansiById(String idInstansi);
+    InstansiResponseDTO updateInstansi(UpdateInstansiRequestDTO requestDTO);
 }
