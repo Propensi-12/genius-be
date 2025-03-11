7d9f8939395f9e3aa6009f4192c12b1aada14d03
Aaron Mario Lin <aaron.mario@ui.ac.id>
2025-03-11 07:21:14 +0700

    merge PBI-4 & 5

--- genius/src/main/java/com/propensi/genius/rest/restservice/ProfileRestServiceImpl.java
+++ genius/src/main/java/com/propensi/genius/rest/restservice/ProfileRestServiceImpl.java
@@ -1,5 +1,7 @@
 package com.propensi.genius.rest.restservice;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.propensi.genius.model.EndUser;
+import com.propensi.genius.repository.EndUserDb;
+import com.propensi.genius.rest.restdto.request.UpdateProfileRequestDTO;
 import com.propensi.genius.rest.restdto.response.UserResponseDTO;
 import com.propensi.genius.security.jwt.JwtUtils;
 import com.propensi.genius.security.service.UserDetailsServiceImpl;
@@ -18,6 +22,9 @@ public class ProfileRestServiceImpl implements ProfileRestService {
     
     @Autowired
     private EndUserService endUserService;
+
+    @Autowired
+    private EndUserDb endUserDb;
 
     @Autowired
     private JwtUtils jwtUtils;
@@ -45,6 +52,35 @@ public class ProfileRestServiceImpl implements ProfileRestService {
         } else {
             throw new IllegalStateException("Tidak dapat menemukan pengguna dalam konteks keamanan.");
         }
+    }
+
+    @Override
+    public UserResponseDTO updateProfileDetails(UpdateProfileRequestDTO updateDTO){
+        Optional<EndUser> user = endUserService.getUserById(updateDTO.getId());
+
+        if(user.isEmpty()){
+            throw new IllegalStateException("Pengguna tidak ditemukan");
+        }
+
+        EndUser selectedUser = user.get();
+
+        selectedUser.setDisplayName(updateDTO.getDisplayName());
+        selectedUser.setName(updateDTO.getName());
+        selectedUser.setNomorHp(updateDTO.getNomorHp());
+        selectedUser.setAlamat(updateDTO.getAlamat());
+
+        endUserDb.save(selectedUser);
+
+        return new UserResponseDTO(
+            selectedUser.getId(),
+            selectedUser.getEmail(),
+            selectedUser.getRole(),
+            selectedUser.getDisplayName(),
+            selectedUser.getName(),
+            selectedUser.getNomorHp(),
+            selectedUser.getAlamat(),
+            selectedUser.isFirstLogin()
+        );
     }
 
 }
